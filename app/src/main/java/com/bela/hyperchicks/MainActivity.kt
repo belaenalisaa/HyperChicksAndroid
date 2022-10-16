23a2e06a586264185d306adaaeebe93ab0106275
belaenalisa <belaenalisa61@gmail.com>
2022-10-16 19:58:42 +0700

    update

--- app/src/main/java/com/bela/hyperchicks/MainActivity.kt
+++ app/src/main/java/com/bela/hyperchicks/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.bela.hyperchicks
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import androidx.core.view.get
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import com.bela.hyperchicks.activity.LoginActivity
 import com.bela.hyperchicks.activity.MasukActivity
-import com.bela.hyperchicks.fragment.AboutFragment
 import com.bela.hyperchicks.fragment.AkunFragment
 import com.bela.hyperchicks.fragment.HomeFragment
+import com.bela.hyperchicks.fragment.KeranjangFragment
 import com.bela.hyperchicks.fragment.MenuFragment
 import com.bela.hyperchicks.helper.SharedPref
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -22,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
     private val fragmentHome: Fragment = HomeFragment()
     private val fragmentMenu: Fragment = MenuFragment()
-    private val fragmentAbout: Fragment = AboutFragment()
+    private val fragmentKeranjang: Fragment = KeranjangFragment()
     private val fragmentAkun: Fragment = AkunFragment()
     private val fm: FragmentManager = supportFragmentManager
     private var active: Fragment = fragmentHome
@@ -46,7 +43,7 @@ class MainActivity : AppCompatActivity() {
     fun setUpBottomNav(){
         fm.beginTransaction().add(R.id.nav_host_fragment, fragmentHome).show(fragmentHome).commit()
         fm.beginTransaction().add(R.id.nav_host_fragment, fragmentMenu).hide(fragmentMenu).commit()
-        fm.beginTransaction().add(R.id.nav_host_fragment, fragmentAbout).hide(fragmentAbout)
+        fm.beginTransaction().add(R.id.nav_host_fragment, fragmentKeranjang).hide(fragmentKeranjang)
             .commit()
         fm.beginTransaction().add(R.id.nav_host_fragment, fragmentAkun).hide(fragmentAkun).commit()
 
@@ -65,7 +62,7 @@ class MainActivity : AppCompatActivity() {
                     callFragment(1,fragmentMenu)
                 }
                 R.id.navigation_about -> {
-                    callFragment(2,fragmentAbout)
+                    callFragment(2,fragmentKeranjang)
                 }
                 R.id.navigation_akun -> {
                     if (s.getStatusLogin()){
--- app/src/main/java/com/bela/hyperchicks/activity/LodingScreenActivity.kt
+++ app/src/main/java/com/bela/hyperchicks/activity/LodingScreenActivity.kt
@@ -1,11 +1,7 @@
 package com.bela.hyperchicks.activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import com.bela.hyperchicks.MainActivity
 import com.bela.hyperchicks.R
 
 class LodingScreenActivity : AppCompatActivity() {
@@ -13,12 +9,6 @@ class LodingScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loding_screen)
 
-        val handler = Handler(Looper.getMainLooper())
-        handler.postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
 
     }
 }
